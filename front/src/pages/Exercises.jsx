5a7e56661d3a9a732402f62a8a4a45166b7b01d5
Miroslava Kostic <mira.kostic01@gmail.com>
2024-08-19 20:24:00 +0200

    Exercise stranica sa vezbama

--- front/src/pages/Exercises.jsx
+++ front/src/pages/Exercises.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
+import server from "../server";
+import Naslov from "../components/Naslov";
+import {Col, Row} from "react-bootstrap";
+import {GiMuscleFat, GiMuscleUp} from "react-icons/gi";
 const Exercises = () => {
+
+    const [exercises, setExercises] = useState([]);
+
+    useEffect(() => {
+        server.get('exercises')
+            .then(response => {
+                console.log(response.data);
+                setExercises(response.data.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+
     return (
         <div>
-            Exercises
+            <Naslov title="Sve vezbe koje cesto izvodimo u teretani" subtitle="Mozda vama neka bude zanimljiva" />
+            <Row className="m-3">
+
+            {
+                exercises.map(exercise => {
+                    return (
+                        <Col key={exercise.id} md={3}>
+                            <h1><GiMuscleUp size={40} />{exercise.name}</h1>
+                            <p>{exercise.description}</p>
+                        </Col>
+                    )
+                })
+            }
+            </Row>
         </div>
     );
 };
